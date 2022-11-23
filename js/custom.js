63ae69bdd03bbb4376abf1052559d6a3d619f23e
Ruochen Wu <97808991+Rc-W024@users.noreply.github.com>
2022-11-23 10:12:20 +0100

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,4 @@
 $(document).ready(function () {
-    setMenuColor();
     var SidebarAnim = new TimelineLite({paused : true});
     SidebarAnim
         .to($(".social-icons, #main-nav"), 0.2, {left : 0})
@@ -18,7 +17,6 @@ $(document).ready(function () {
     $(".social-icons, #main-nav a, #main").on("click", function () {
         SidebarAnim.reverse();
         SidebarAnim.isOpen = false;
-        setMenuColor($(this).attr("href"));
     });
 
     $("ul.timeline").children().eq(0)
@@ -36,28 +34,4 @@ $(document).ready(function () {
         wheelPropagation : true,
         wheelSpeed       : 50
     });
-    
-    function setMenuColor(hash) {
-        var loc = window.location.href;
-
-        if (hash || loc.indexOf('#') > 0) {
-            var hash = hash ? hash : loc.substring(loc.indexOf('#')),
-                par = $('a[href="' + hash + '"]').parent(),
-                sib = par.siblings();
-
-            for (var i = 0; i < sib.length; ++i) {
-                var elem = $(sib[i]);
-                if (!elem.hasClass('external')) {
-                    elem.removeClass();
-                }
-            }
-            if (!par.hasClass('external')) {
-                par.addClass('currentmenu');
-            }
- 		ga('send', 'pageview', {
-		 'page': location.pathname + location.search + location.hash
-		});
-        }
-    }
-
 });
