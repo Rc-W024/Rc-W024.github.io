e1897ab1c8addd0e400f2d8e672542a66ff5d648
Ruochen Wu <ruochen.wu@upc.edu>
2026-05-15 16:46:38 +0200

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -4,6 +4,14 @@ $(document).ready(function () {
         .to($(".social-icons, #main-nav"), 0.2, {left : 0})
         .to($("#main"), 0.2, {left : 250, right : "-=250"}, "-=0.2");
 
+	$('#navigation a[href^="#"]').on('click', function (e) {
+    	var target = document.querySelector(this.getAttribute('href'));
+    	if (target) {
+        	e.preventDefault();
+        	target.scrollIntoView({ behavior: 'smooth' });
+    	}
+	});
+
     $("a.mobilemenu").on("click", function () {
         if (SidebarAnim.isOpen) {
             SidebarAnim.reverse();
@@ -19,7 +27,7 @@ $(document).ready(function () {
         SidebarAnim.isOpen = false;
 	var par = $(this).parent();
         var sib = par.siblings();
-        for(var i=0; i<sib.length; i++){
+        for (var i=0; i<sib.length; i++){
 		var elem = $(sib[i]);
         	if (!elem.hasClass('external')) {
                 	elem.removeClass();
@@ -35,7 +43,7 @@ $(document).ready(function () {
         .addClass("open");
 	
     $("ul.timeline").on("click", "li", function () {
-        $this = $(this);
+        const $this = $(this);
         $this.find(".text").slideDown();
         $this.addClass("open");
         $this.siblings('li.open').find(".text").slideUp();
